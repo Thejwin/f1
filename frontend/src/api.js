8d2adeadfb61848dc5101d1739405b587e5ff8f3
Thejwin <thejwin123@gmail.com>
2026-06-28 23:38:37 +0530

    minor chnge

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,17 +1,19 @@
+const ip = "http://127.0.0.1:8000"
+
 export const fetchDriverStandings = () =>
-  fetch(`http://127.0.0.1:8000/driver-standings/`).then((res) => res.json());
+  fetch(`${ip}/driver-standings/`).then((res) => res.json());
 
 export const fetchConstructorStandings = () =>
-  fetch(`http://127.0.0.1:8000/constructor-standings/`).then((res) => res.json());
+  fetch(`${ip}/constructor-standings/`).then((res) => res.json());
 
 export const fetchSchedule = (year) =>
-  fetch(`http://127.0.0.1:8000/schedule?year=${year}`).then((res) => res.json());
+  fetch(`${ip}/schedule?year=${year}`).then((res) => res.json());
 
 export const fetchEventSessions = (year, round) =>
-  fetch(`http://127.0.0.1:8000/schedule/${year}/${round}`).then((res) => res.json());
+  fetch(`${ip}/schedule/${year}/${round}`).then((res) => res.json());
 
 export const fetchSessionResults = (year, round, sessionId) =>
-  fetch(`http://127.0.0.1:8000/schedule/${year}/${round}/${sessionId}/results`).then((res) => res.json());
+  fetch(`${ip}/schedule/${year}/${round}/${sessionId}/results`).then((res) => res.json());
 
 export const fetchRaceLaps = (year, round) =>
-  fetch(`http://127.0.0.1:8000/laps/${year}/${round}`).then((res) => res.json());
+  fetch(`${ip}/laps/${year}/${round}`).then((res) => res.json());
